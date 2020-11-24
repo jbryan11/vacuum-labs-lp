dbfbc1f16427b0fb1a8a9c469dd960f7a79c7c48
John Bryan Delda <mightypirates174@gmail.com>
2020-11-24 19:04:52 +0800

    Adding Products Section

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import React from "react"
 import Layout from "../components/common/layout/layout"
 import SEO from "../components/common/layout/seo"
 import Navigation from "../components/common/navigation/navigation"
-
+import Products from "../components/sections/products"
 import Header from "../components/sections/header"
 import Features from "../components/sections/features"
 import Footer from "../components/sections/footer"
@@ -15,7 +15,7 @@ const IndexPage = () => (
     <Navigation />
     <Header />
     <Features />
-    <Features />
+    <Products />
     <GetStarted />
     <Footer />
   </Layout>
