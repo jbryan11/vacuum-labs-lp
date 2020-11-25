2df6ba2fcfc9a4a26a4a75ab28168299c503b09e
John Bryan Delda <mightypirates174@gmail.com>
2020-11-25 15:25:39 +0800

    hotfix9

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,11 +4,11 @@ import Layout from "../components/common/layout/layout"
 import SEO from "../components/common/layout/seo"
 import Navigation from "../components/common/navigation/navigation"
 import Contact from "../components/sections/contact"
-const Contact =()=>(
+const ContactUs =()=>(
 <Layout>
     <Seo title="Contact Us"/>
     <Navigation />
     <Contact/>
 </Layout>
 )
-export default Contact
+export default ContactUs
