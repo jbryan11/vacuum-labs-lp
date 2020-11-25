9414200a330e4046e1bd5fec64bc3eaed11074e7
John Bryan Delda <mightypirates174@gmail.com>
2020-11-25 15:23:30 +0800

    hotfix8

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,3 +1,14 @@
-export default function Contact(){
-    return <h1>Coming Soon...</h1>
-}
+import React from "react"
+
+import Layout from "../components/common/layout/layout"
+import SEO from "../components/common/layout/seo"
+import Navigation from "../components/common/navigation/navigation"
+import Contact from "../components/sections/contact"
+const Contact =()=>(
+<Layout>
+    <Seo title="Contact Us"/>
+    <Navigation />
+    <Contact/>
+</Layout>
+)
+export default Contact
