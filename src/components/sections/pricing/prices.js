8f2f03607819ca60f122247007f5af1a11c996f7
John Bryan Delda <mightypirates174@gmail.com>
2020-12-01 16:23:07 +0800

    fix: grid spacing reduced column and increase column sizing

--- src/components/sections/pricing/prices.js
+++ src/components/sections/pricing/prices.js
@@ -115,7 +115,7 @@ const Subtitle = styled.h5`
 const FeaturesGrid = styled.div`
   max-width: 800px;
   display: grid;
-  grid-template-columns: 1.5fr 1.5fr 1.5fr 1.5fr;
+  grid-template-columns: 2fr 2fr 2fr;
   margin: 0px auto;
   grid-column-gap: 40px;
   grid-row-gap: 35px;
